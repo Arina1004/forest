356cfdad769712f084b1affe6441b0fe8ca9fe4e
Ilya_Aladiev <aladjev.ilia2010@yandex.ru>
2019-12-07 13:12:58 +0300

    Added consts

---
+++ types/enums.js
@@ -0,0 +1,40 @@
+export const WIN_COST = 10;
+
+export const SUN_POSITION_TOP = 0;
+export const SUN_POSITION_RIGHT = 1;
+export const SUN_POSITION_BOTTOM = 2;
+export const SUN_POSITION_LEFT = 3;
+
+export const CELL_CONTENT_EMPTY = 0;
+export const CELL_CONTENT_SEED = 1;
+export const CELL_CONTENT_SMALL = 2;
+export const CELL_CONTENT_MEDIUM = 3;
+export const CELL_CONTENT_LARGE = 4;
+
+export const shadows = {
+    CELL_CONTENT_EMPTY: 0,
+    CELL_CONTENT_SEED: 0,
+    CELL_CONTENT_SMALL: 1,
+    CELL_CONTENT_MEDIUM: 2,
+    CELL_CONTENT_LARGE: 3
+};
+
+export const prices = {
+    CELL_CONTENT_SEED: 1,
+    CELL_CONTENT_SMALL: 1,
+    CELL_CONTENT_MEDIUM: 2,
+    CELL_CONTENT_LARGE: 3
+};
+
+export const upgrades = {
+    CELL_CONTENT_SMALL: 1,
+    CELL_CONTENT_MEDIUM: 2,
+    CELL_CONTENT_LARGE: 3
+};
+
+export const energy = {
+    CELL_CONTENT_SEED: 0,
+    CELL_CONTENT_SMALL: 1,
+    CELL_CONTENT_MEDIUM: 2,
+    CELL_CONTENT_LARGE: 3
+}
